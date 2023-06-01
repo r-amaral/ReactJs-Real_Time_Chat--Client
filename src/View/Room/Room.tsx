54af84f2c60116887cb62a304c195bb06b94c9f0
Ruan Oliveira <ruan.oliveira_compasso@terrainvestimentos.com.br>
2023-05-31 22:32:37 -0300

    feat: crinado lógica para conectar sockets ao back

--- src/View/Room/Room.tsx
+++ src/View/Room/Room.tsx
@@ -5,45 +5,119 @@ import Message from "../Components/Message";
 import { InputWrapper, MessagesWrapper, ProfileWrapper } from "./styles";
 import Profile from "../Components/Profile";
 
+import io, { Socket } from "socket.io-client";
+import * as uuid from "uuid";
+
+interface PayloadProps {
+  id?: string;
+  author: string;
+  message: string;
+  time?: string;
+  room?: string;
+}
+
 export const Room = () => {
+  const port = 3001;
+
+  const socketIORef = React.useRef<Socket>();
+  const listRef = React.useRef<HTMLDivElement>(null);
+
+  const [author, setAuthor] = React.useState("");
+  const [message, setMessage] = React.useState("");
+  const [messages, setMessages] = React.useState<PayloadProps[]>([]);
+
+  const handleSendMessage = async () => {
+    if (author && message) {
+      const messageData: PayloadProps = {
+        id: uuid.v4(),
+        author,
+        message,
+        time: timeFormart(),
+      };
+
+      socketIORef.current?.emit("msgToClient", messageData);
+      setMessage("");
+    }
+  };
+
+  const receivedMessage = (newmessage: PayloadProps) => {
+    const messageData: PayloadProps = {
+      id: uuid.v4(),
+      author: newmessage.author,
+      message: newmessage.message,
+      time: timeFormart(),
+    };
+
+    setMessages([...messages, messageData]);
+  };
+
+  const joinRoom = () => {
+    if (!author) {
+      const joined = {
+        id: uuid.v4(),
+        author,
+      };
+
+      socketIORef.current?.emit("joined-room", joined);
+      setAuthor(joined.author);
+    }
+  };
+
+  const timeFormart = () => {
+    const time = new Date();
+    const hour = time.getHours();
+    const min = time.getMinutes();
+
+    return (hour < 10 ? "0" + hour : hour) + ":" + (min < 10 ? "0" + min : min);
+  };
+
+  React.useEffect(() => {
+    socketIORef.current = io(`http://localhost:${port}`);
+    socketIORef.current.on("connect", () => {
+      console.log("conectado");
+    });
+
+    socketIORef.current.on("msgToClient", (message: PayloadProps) =>
+      receivedMessage(message)
+    );
+  }, [socketIORef, messages, author, message]);
+
+  React.useEffect(() => {
+    if (listRef.current) {
+      listRef.current.scrollTop = listRef.current.scrollHeight;
+    }
+  }, [messages]);
+
   return (
     <PageTemplateWrapper>
+      <div>
+        <input
+          type="text"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          placeholder="Enter name..."
+        />
+        <button onClick={joinRoom}>join</button>
+      </div>
       <ProfileWrapper>
-        <Profile profileName="Ruan Oliveira" />
+        <Profile profileName={author} />
       </ProfileWrapper>
-      <MessagesWrapper>
-        <Message
-          user={false}
-          name="Ruan"
-          time="11:32"
-          message="primeira mensagem"
-        />
-        <Message user name="vanessa" time="11:44" message="segunda mensagem" />
-        <Message user name="vanessa" time="11:44" message="segunda mensagem" />
-        <Message user name="vanessa" time="11:44" message="segunda mensagem" />
-        <Message user name="vanessa" time="11:44" message="segunda mensagem" />
-        <Message user name="vanessa" time="11:44" message="segunda mensagem" />
-        <Message user name="vanessa" time="11:44" message="segunda mensagem" />
-        <Message user name="vanessa" time="11:44" message="segunda mensagem" />
-        <Message user name="vanessa" time="11:44" message="segunda mensagem" />
-        <Message user name="vanessa" time="11:44" message="segunda mensagem" />
-        <Message user name="vanessa" time="11:44" message="segunda mensagem" />
-        <Message user name="vanessa" time="11:44" message="segunda mensagem" />
-        <Message user name="vanessa" time="11:44" message="segunda mensagem" />
-        <Message
-          user={false}
-          name="Ruan"
-          time="11:55"
-          message="Terceira Mensagem"
-        />
+      <MessagesWrapper ref={listRef}>
+        {messages.map((message) => (
+          <Message
+            user={message.author === author}
+            name={message.author}
+            time={message.time}
+            message={message.message}
+          />
+        ))}
       </MessagesWrapper>
       <InputWrapper>
         <InputMessage
-          onChange={() => {
-            null;
-          }}
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
           onClick={() => {
-            null;
+            handleSendMessage();
           }}
         />
       </InputWrapper>
