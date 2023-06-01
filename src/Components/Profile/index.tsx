f6686c6061dfeaeec6d0bca3aaa700e6bac4dada
Ruan Oliveira <ruan.oliveira_compasso@terrainvestimentos.com.br>
2023-06-01 13:03:07 -0300

    feat: criando lógica para randomizar imagem do profile

--- src/Components/Profile/index.tsx
+++ src/Components/Profile/index.tsx
@@ -1,18 +1,33 @@
+import React from "react";
 import * as S from "./styles";
-import DogImg from "../../assets/dog.jpg";
+import { profileImage } from "./profileImage";
 import { ProfileProps } from "../../Interfaces";
 
-const Profile = ({ profileName }: ProfileProps) => (
-  <S.ProfileWrapper>
-    <S.ProfileImage src={DogImg} />
-    <S.ProfileContent>
-      <S.ProfileName>{profileName}</S.ProfileName>
-      <S.ProfileStatus>
-        <S.ProfileBall />
-        Online
-      </S.ProfileStatus>
-    </S.ProfileContent>
-  </S.ProfileWrapper>
-);
+const Profile = ({ profileName }: ProfileProps) => {
+  const [img, setImg] = React.useState<string>();
 
+  const getRandomImageProfile = () => {
+    const ramdomImage =
+      profileImage[Math.floor(Math.random() * profileImage.length)];
+
+    setImg(ramdomImage);
+  };
+
+  React.useEffect(() => {
+    getRandomImageProfile();
+  }, []);
+
+  return (
+    <S.ProfileWrapper>
+      <S.ProfileImage src={img} />
+      <S.ProfileContent>
+        <S.ProfileName>{profileName}</S.ProfileName>
+        <S.ProfileStatus>
+          <S.ProfileBall />
+          Online
+        </S.ProfileStatus>
+      </S.ProfileContent>
+    </S.ProfileWrapper>
+  );
+};
 export default Profile;
---
+++ src/Components/Profile/profileImage.ts
@@ -0,0 +1,21 @@
+import Dog1 from "../../assets/profileImage/dog.jpg";
+import Dog2 from "../../assets/profileImage/do2.jpeg";
+import Dog3 from "../../assets/profileImage/dog3.jpg";
+import Dog4 from "../../assets/profileImage/dog4.jpg";
+import Dog5 from "../../assets/profileImage/dog5.jpg";
+import Dog6 from "../../assets/profileImage/dog6.jpeg";
+import Dog7 from "../../assets/profileImage/dog7.jpg";
+import Dog8 from "../../assets/profileImage/dog8.jpg";
+import Dog10 from "../../assets/profileImage/dog10.jpg";
+
+export const profileImage = [
+  Dog1,
+  Dog2,
+  Dog3,
+  Dog4,
+  Dog5,
+  Dog6,
+  Dog7,
+  Dog8,
+  Dog10,
+];
