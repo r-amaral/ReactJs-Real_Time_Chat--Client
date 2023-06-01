7369990b106380f24f6322f95292e19f9a898e53
Ruan Oliveira <ruan.oliveira_compasso@terrainvestimentos.com.br>
2023-05-31 22:30:59 -0300

    feat: adicionando value ao input message

--- src/View/Components/InputMessage/index.tsx
+++ src/View/Components/InputMessage/index.tsx
@@ -4,19 +4,19 @@ import { Input, InputWrapper, SendMessage } from "./styles";
 interface InputMessageProps {
   onChange(e: React.ChangeEvent<HTMLInputElement>): void;
   onClick(e: React.MouseEvent<HTMLElement>): void;
+  value: string;
 }
 
-const InputMessage = ({ onChange, onClick }: InputMessageProps) => {
-  return (
-    <InputWrapper>
-      <Input
-        type="text"
-        placeholder="Digite sua mensagem"
-        onChange={onChange}
-      />
-      <SendMessage onClick={onClick} />
-    </InputWrapper>
-  );
-};
+const InputMessage = ({ onChange, onClick, value }: InputMessageProps) => (
+  <InputWrapper>
+    <Input
+      value={value}
+      type="text"
+      placeholder="Digite sua mensagem"
+      onChange={onChange}
+    />
+    <SendMessage onClick={onClick} />
+  </InputWrapper>
+);
 
 export default InputMessage;
