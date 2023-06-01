447a24e5b99dc869a23853b0f0a8fcbbc08d1084
Ruan Oliveira <ruan.oliveira_compasso@terrainvestimentos.com.br>
2023-05-31 21:06:07 -0300

    feat: criando componente de mensagem

---
+++ src/View/Components/Message/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  MessageHeaderContainer,
+  MessageHeaderItem,
+  MessageText,
+  MessageWrapper,
+} from "./styles";
+
+interface MessageProps {
+  user: boolean;
+  time: string;
+  name: string;
+  message: string;
+}
+
+const Message = ({ user, time, name, message }: MessageProps) => {
+  return (
+    <MessageWrapper localUser={user}>
+      <MessageHeaderContainer localUser={user}>
+        <MessageHeaderItem>
+          {name} - {time}
+        </MessageHeaderItem>
+      </MessageHeaderContainer>
+      <MessageText localUser={user}>{message}</MessageText>
+    </MessageWrapper>
+  );
+};
+
+export default Message;
